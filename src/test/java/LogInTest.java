2e20c995f0de34230c63c1e6fa639c3ec20f41e9
MARA <cmarateodora@gmail.com>
2023-08-14 17:49:54 +0300

    Created the pages, utils, constants, BooksTest.

--- src/test/java/LogInTest.java
+++ src/test/java/LogInTest.java
@@ -1,8 +1,6 @@
 import constants.Constants;
 import constants.ErrorMessage;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
